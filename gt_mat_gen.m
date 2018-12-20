6b521d309185c6a84f5236b1c86dbbe9ab2459f6
Nikhil Jindal <nik.jindal20@gmail.com>
2018-12-20 16:55:36 +0530

    Add files via upload

---
+++ gt_mat_gen.m
@@ -0,0 +1,24 @@
+%Generates ground truth .mat files for each image file using head
+%annotations
+
+clear all
+clc
+close all
+filePattern = fullfile('images', '/*.jpg');
+ImageFiles = dir(filePattern);
+n = length(ImageFiles);
+read_path = 'images/';
+store_path = 'ground-truth/';
+t = 0;                         %number of files initially in training set
+
+for i=1:n
+    disp(i);
+    im = imread([read_path 'IMG_' num2str(t+i) '.jpg']); 
+    figure
+    imshow(im)
+    [x,y] = getpts;
+    image_info{1,1}.location = [x y];
+    image_info{1,1}.number = size(x,1);
+    save([store_path 'GT_IMG_' num2str(t+i) '.mat'], 'image_info')
+    close
+end
